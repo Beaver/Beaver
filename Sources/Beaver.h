ac4f3167dadfcefc78e5251bb59c1befb463ef52
Theophane Rupin <theophane.rupin@gmail.com>
2017-04-08 08:55:36 +0200

    added xcodeproj and first targets

---
+++ Sources/Beaver.h
@@ -0,0 +1,19 @@
+//
+//  Beaver.h
+//  Beaver
+//
+//  Created by Theophane Rupin on 4/8/17.
+//  Copyright © 2017 beaver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Beaver.
+FOUNDATION_EXPORT double BeaverVersionNumber;
+
+//! Project version string for Beaver.
+FOUNDATION_EXPORT const unsigned char BeaverVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Beaver/PublicHeader.h>
+
+
