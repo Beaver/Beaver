c538b785ab2bcdd4d41785a55553a9ddb2a982f8
Theophane Rupin <theophane.rupin@gmail.com>
2017-04-08 16:32:51 +0200

    added subscribing

--- Beaver/Type/Subscriber.swift
+++ Beaver/Type/Subscriber.swift
@@ -46,3 +46,47 @@ extension Store.Subscriber: CustomDebugStringConvertible {
         return "\(name)"
     }
 }
+
+/// A type responsible for handling state updates
+public protocol Subscribing: class {
+    associatedtype ActionType: Action
+
+    /// Name automatically given to the store when subscribing
+    var subscriptionName: String { get }
+
+    /// Should store be able to retain an instance of the subscribing class or not
+    var isSubscriptionWeak: Bool { get }
+
+    func stateDidUpdate(source: ActionEnvelop<ActionType>?,
+                        oldState: Store<ActionType>.StateType?,
+                        newState: Store<ActionType>.StateType)
+}
+
+extension Subscribing {
+    public var subscriptionName: String {
+        return String(describing: type(of: self))
+    }
+
+    var isSubscriptionWeak: Bool {
+        return true
+    }
+
+    /// Subscribes to a store.
+    public func subscribe(to store: Store<ActionType>) {
+        if isSubscriptionWeak {
+            store.subscribe(name: subscriptionName) { [weak self] source, oldState, newState in
+                self?.stateDidUpdate(source: source, oldState: oldState, newState: newState)
+            }
+        } else {
+            store.subscribe(name: subscriptionName) { source, oldState, newState in
+                self.stateDidUpdate(source: source, oldState: oldState, newState: newState)
+            }
+        }
+    }
+}
+
+extension Subscribing where Self: UIViewController {
+    var isSubscriptionWeak: Bool {
+        return false
+    }
+}
